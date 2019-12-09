75ab180e32890c7e393aab2cc3602d5f18756ea8
lifesscholar <n_granite@hotmail.com>
2019-12-09 11:12:19 -0500

    Added methods #find_path and #trace_path_back to KnightPathFinder.rb

--- skeleton/lib/KnightPathFinder.rb
+++ skeleton/lib/KnightPathFinder.rb
@@ -66,6 +66,20 @@ class KnightPathFinder
         new_moves
     end
 
+    def find_path(end_pos)
+        end_node = @root_node.dfs(end_pos)
+
+        self.trace_path_back(end_node)
+    end
+
+    def trace_path_back(node)
+        return [@root_node.value] if node == @root_node
+        
+        path_arr = trace_path_back(node.parent)
+        path_arr.push(node.value)
+
+        path_arr
+    end
 
 end 
 
@@ -74,5 +88,12 @@ if __FILE__ == $PROGRAM_NAME
     # p kpf.new_moves_positions([0, 0])
     # kpf.considered_positions
 
-    p kpf.root_node
+    # p kpf.root_node
+    # p kpf.root_node.print_tree
+
+    p kpf.find_path([1, 2])
+    p kpf.find_path([2, 4])
+
+    p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+    p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
 end
